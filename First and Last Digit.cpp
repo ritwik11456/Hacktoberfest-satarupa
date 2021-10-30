3118b01f0db5ee016d620d9761b44ddf42434f05
ritwik11456 <93448126+ritwik11456@users.noreply.github.com>
2021-10-31 00:09:50 +0530

    Create First and Last Digit.cpp

---
+++ First and Last Digit.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int t;
+    cin >> t;
+    
+    for(int i=0;i<t;i++)
+    {
+        int n,ans=0,first=0;
+        cin >> n;
+        ans += n%10;
+        while(n>0)
+        {
+            first=n%10;
+            n/=10;
+        }
+        ans+=first;
+        cout << ans <<endl;
+    }
+    return 0;
+}
